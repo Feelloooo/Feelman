7004fac25e7169cc34e817c3b1afb726eca3f500
seokwooyoung <dndud0506@naver.com>
2021-06-23 19:06:15 +0900

    woo

--- app/src/main/java/com/example/feelman/MainActivity.java
+++ app/src/main/java/com/example/feelman/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.feelman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper v_fllipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int images[] = {
+                R.drawable.photo1,
+                R.drawable.photo2,
+                R.drawable.photo3
+        };
+
+        v_fllipper = findViewById(R.id.image_slide);
+
+        for(int image : images) {
+            fllipperImages(image);
+        }
+
+    }
+    public void fllipperImages(int image) {
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_fllipper.addView(imageView);      // 이미지 추가
+        v_fllipper.setFlipInterval(4000);       // 자동 이미지 슬라이드 딜레이시간(4000 당 1초)
+        v_fllipper.setAutoStart(true);          // 자동 시작 유무 설정
+
+        // animation
+        v_fllipper.setInAnimation(this,android.R.anim.slide_in_left);
+        v_fllipper.setOutAnimation(this,android.R.anim.slide_out_right);
     }
 }
