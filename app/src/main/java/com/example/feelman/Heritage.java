940816cdf43edceb9888a04eff7c3bed74170dc9
seokwooyoung <dndud05066@gmail.com>
2021-07-01 19:16:36 +0900

    21.06.30 Database 불러오기

--- app/src/main/java/com/example/feelman/Heritage.java
+++ app/src/main/java/com/example/feelman/Heritage.java
@@ -4,6 +4,9 @@ public class Heritage {
     public String name; //사용자명
     public String address; //사용자 주소
     public String description; // 설명
+    public String detail;
+    public String era;
+    public String place;
 
     public String getName() {
         return name;
@@ -22,5 +25,25 @@ public class Heritage {
     }
     public void setDescription(String description) {
         this.description = description;
+    }
+    public String getEra() {
+        return era;
+    }
+    public void setEra(String era) {
+        this.era = era;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+    public void setPlace(String place) {
+        this.place = place;
     }
 }
