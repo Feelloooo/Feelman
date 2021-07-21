c43fd2247487485023940fe50e9fb7f3d35d34ff
seokwooyoung <dndud05066@gmail.com>
2021-07-21 14:34:04 +0900

    21.07.20 마무리

--- app/src/main/java/com/example/feelman/Activity_program.java
+++ app/src/main/java/com/example/feelman/Activity_program.java
@@ -3,7 +3,11 @@ package com.example.feelman;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_program extends AppCompatActivity {
 
@@ -13,6 +17,33 @@ public class Activity_program extends AppCompatActivity {
         setContentView(R.layout.activity_program);
 
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("박물관 프로그램");
+        actionBar.setTitle("초중고 체험프로그램");
+
+        Button button1 = findViewById(R.id.button1); //다른 버튼 사용할 때 변수명 바꿔서 사용
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Activity_program_1.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2); //다른 버튼 사용할 때 변수명 바꿔서 사용
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Activity_program_2.class);
+                startActivity(intent);
+            }
+        });
+        Button button3 = findViewById(R.id.button3); //다른 버튼 사용할 때 변수명 바꿔서 사용
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Activity_program_3.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/feelman/Activity_program_3.java
+++ app/src/main/java/com/example/feelman/Activity_program_3.java
@@ -1,8 +1,13 @@
 package com.example.feelman;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_program_3 extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class Activity_program_3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program_3);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("HIGH 큐레이터");
+
+        Button button = findViewById(R.id.button); //다른 버튼 사용할 때 변수명 바꿔서 사용
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ggoomgil.go.kr/index.jsp"));
+                startActivity(intent);
+            }
+        });
     }
 }
